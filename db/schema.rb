2ee0a6dbe94e21fe5c0c17c3bfecfa535d6e8a1f
Hama99o <hmmshl@gmail.com>
2024-11-09 20:53:50 +0100

    index_comments_on_commentable removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_09_183112) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_09_195205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,7 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_09_183112) do
     t.jsonb "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -14,7 +14,6 @@
 #
 # Indexes
 #
-#  index_comments_on_commentable                          (commentable_type,commentable_id)
 #  index_comments_on_commentable_type_and_commentable_id  (commentable_type,commentable_id)
 #  index_comments_on_parent_id                            (parent_id)
 #  index_comments_on_user_id                              (user_id)
