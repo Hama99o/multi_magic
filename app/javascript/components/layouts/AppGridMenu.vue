09ea28f079f3c04c394a64b2c376413d860693ae
Hama99o <hmmshl@gmail.com>
2024-12-21 01:49:20 +0100

    home page reset

--- app/javascript/components/layouts/AppGridMenu.vue
+++ app/javascript/components/layouts/AppGridMenu.vue
@@ -1,20 +1,13 @@
 <template>
-  <dropdown
-    v-model="menu"
-    :offset="8"
-    bottom
-  >
-    <template v-slot:activator="{ props }">
+  <dropdown v-model="menu" :offset="8" bottom>
+    <template #activator="{ props }">
       <v-toolbar class="bg-surface">
         <v-toolbar-items>
-          <v-btn
-            icon
-            v-bind="props"
-          >
+          <v-btn icon v-bind="props">
             <v-icon class="text-primary">mdi-view-grid-outline</v-icon>
           </v-btn>
         </v-toolbar-items>
-      </v-toolbar >
+      </v-toolbar>
     </template>
 
     <template #dropdown>
@@ -25,12 +18,18 @@
               v-for="app in menuItems"
               :key="app.title"
               cols="6"
-              class="group flex items-center hover:bg-background justify-center cursor-pointer p-4 transition-transform duration-200 transform hover:scale-105 hover:bg-info"
+              class="group flex transform cursor-pointer items-center justify-center p-4 transition-transform duration-200 hover:scale-105 hover:bg-background hover:bg-info"
               @click="goToApp(app.routeName, app.query)"
             >
-              <div class="flex flex-col items-center text-center gap-3">
-                <v-icon :icon="app.icon" class="text-5xl group-hover:text-indigo-500 transition-colors duration-200" :alt="app.title"/>
-                <div class="text-center text-lg text-nowrap font-semibold group-hover:text-indigo-500 transition-colors duration-200">
+              <div class="flex flex-col items-center gap-3 text-center">
+                <v-icon
+                  :icon="app.icon"
+                  class="text-5xl transition-colors duration-200 group-hover:text-indigo-500"
+                  :alt="app.title"
+                />
+                <div
+                  class="text-nowrap text-center text-lg font-semibold transition-colors duration-200 group-hover:text-indigo-500"
+                >
                   {{ app.title }}
                 </div>
               </div>
@@ -47,8 +46,8 @@ import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import Dropdown from '@/components/button/Dropdown.vue';
 
-const menu = ref(false)
-const router = useRouter()
+const menu = ref(false);
+const router = useRouter();
 
 const props = defineProps({
   isSuperAdmin: { type: Boolean, required: true },
@@ -56,24 +55,53 @@ const props = defineProps({
 });
 
 const apps = ref([
+  {
+    title: 'Safezone',
+    routeName: 'safezone_app_passwords',
+    icon: 'mdi mdi-key-variant',
+    appName: 'SafezoneApp',
+    query: {},
+  },
+  {
+    title: 'My Notes',
+    routeName: 'notes',
+    icon: 'mdi mdi-note',
+    appName: 'NoteApp',
+    query: { page: 'all_notes' },
+  },
+  {
+    title: 'My Finance',
+    routeName: 'expenses',
+    icon: 'mdi mdi-cash-multiple',
+    appName: 'MyFinanceApp',
+    query: {},
+  },
+  {
+    title: 'My Contacts',
+    routeName: 'contacts',
+    icon: 'mdi mdi-account-box',
+    appName: 'ContactApp',
+    query: {},
+  },
+  {
+    title: 'My Blog',
+    routeName: 'articles',
+    icon: 'mdi mdi-school',
+    appName: 'BlogApp',
+    query: {},
+  },
   { title: 'Users', routeName: 'users', icon: 'mdi-account-group', appName: 'Users', query: {} },
-  { title: 'My Notes', routeName: 'notes', icon: 'mdi mdi-note', appName: 'NoteApp', query: { page: 'all_notes' } },
-  { title: 'My Finance', routeName: 'expenses', icon: 'mdi mdi-cash-multiple', appName: 'MyFinanceApp', query: {} },
-  { title: 'My Contacts', routeName: 'contacts', icon: 'mdi mdi-account-box', appName: 'ContactApp', query: {} },
-  { title: 'My Blog', routeName: 'articles', icon: 'mdi mdi-school', appName: 'BlogApp', query: {} }
-])
+]);
 
 const goToApp = (routeName, query) => {
   router.push({
     name: routeName,
-    query: query
-  })
-  menu.value = false
-}
+    query: query,
+  });
+  menu.value = false;
+};
 
 const menuItems = computed(() => {
-  return apps.value.filter((item) => props.currentUser?.applications?.includes(item.appName))
+  return apps.value.filter((item) => props.currentUser?.applications?.includes(item.appName));
 });
 </script>
-
-
